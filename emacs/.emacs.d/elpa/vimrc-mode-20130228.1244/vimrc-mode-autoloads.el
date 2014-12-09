698f39c9764f7eb7d2350db5339966c7913a2d86
da0shi <dev@da0shi.com>
2014-12-09 19:18:41 +0900

    Set to ignore emacs packages

--- emacs/.emacs.d/elpa/vimrc-mode-20130228.1244/vimrc-mode-autoloads.el
+++
@@ -1,29 +0,0 @@
-;;; vimrc-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (vimrc-mode) "vimrc-mode" "vimrc-mode.el" (21340
-;;;;;;  40571 145746 742000))
-;;; Generated autoloads from vimrc-mode.el
-
-(autoload 'vimrc-mode "vimrc-mode" "\
-Major mode for editing `vimrc', `xxx.vim' and `.exrc' configuration files.
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("vimrc-mode-pkg.el") (21340 40571 242068
-;;;;;;  136000))
-
-;;;***
-
-(provide 'vimrc-mode-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; vimrc-mode-autoloads.el ends here
