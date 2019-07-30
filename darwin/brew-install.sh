b134b87e375b1d6544e8b93484d73c3882b89c51
takayukioda <takayuki@odashi.me>
2019-07-30 11:12:51 +0900

    feat: update install files

--- darwin/brew-install.sh
+++ darwin/brew-install.sh
@@ -2,26 +2,34 @@
 
 which brew >/dev/null || { echo "brew not found: $?" ; exit 1; }
 
-brew cask install java
+brew cask install \
+	java \
+	google-chrome \
+	firefox \
+	slack \
+	insomnia \
+	visual-studio-code \
+	sublime-text \
+
 brew install \
-  awscli \
-  bash-completion \
-  bash-git-prompt \
-  direnv \
-  ffmpeg \
-  fzf \
-  git \
-  go \
-  jq \
-  node \
-  openssl \
-  pgcli \
-  sbt \
-  scala \
-  terraform \
-  tig \
-  tmux \
-  unar \
-  vim \
-  vim \
-  yarn \
+	awscli \
+	bash-completion \
+	bash-git-prompt \
+	direnv \
+	ffmpeg \
+	fzf \
+	git \
+	go \
+	jq \
+	node \
+	openssl \
+	pgcli \
+	sbt \
+	scala \
+	terraform \
+	tig \
+	tmux \
+	unar \
+	vim \
+	vim \
+	yarn \
